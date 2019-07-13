8d9723fba306a9efbb75d63874968a33220dba39
Anh Dao <addao823@gmail.com>
2019-07-13 13:30:30 -0700

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-#Memory Game
-
-## About:
-- A simple React project game
-- Try to match all the cards by clicking on the "?". If it matched, it will stay open, if not, it will flip again.
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app
---
+++ precache-manifest.480c896bf4ef7681013ffeb7e8033a23.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76becd2ad0e3ca352f61cf809492b9a8",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/memory-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "807139f81310956a3c12",
+    "url": "/memory-game/static/js/main.b994469b.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
